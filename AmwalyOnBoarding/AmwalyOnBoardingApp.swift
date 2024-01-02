e2a9572580209b0087cd7c7879e23d7a58fd6d43
SaudAlhafith <SaudAlhafith@gmail.com>
2024-01-02 14:24:17 +0300

    Initial Commit

---
+++ AmwalyOnBoarding/AmwalyOnBoardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmwalyOnBoardingApp.swift
+//  AmwalyOnBoarding
+//
+//  Created by saudAlhafith on 20/06/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct AmwalyOnBoardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AmwalyOnBoarding/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AmwalyOnBoarding
+//
+//  Created by saudAlhafith on 20/06/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
